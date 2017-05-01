e9bd0457fed9169895cf308e1bde3013a31cae4b
Chris <chris.thiele@haw-hamburg.de>
2017-05-01 16:12:28 +0200

    fast fertig D:

--- src/de/max/chris/Main.java
+++ src/de/max/chris/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    //[fixed #2]
+    // [fixed #2]
     public boolean isAList(ListenElement l){
       boolean success = false;
 
@@ -20,170 +20,42 @@ public class Main {
       return success;
     }
 
+    // [fixed #2]
     public ListenElement diffList(ListenElement liste1, ListenElement liste2){
-        boolean run = true;
+        boolean nextNull = false;
 
-        // Die Methode durchläuft alle Elemente der übergebenen Liste.
-        while(run){
-            // Wenn das Ende der Liste erreicht ist, bricht die Schleife am Ende ab
-            // und gibt die Ergebnisliste zurück.
-            if(liste2 == null){
-                run = false;
-            }
+        if(liste1 == null){
+            nextNull = true;
+        }
 
-            // Es wird überprüft, ob das zu überprüfende Element aus der 2. Liste (L2)
-            // in 1. Liste (L1) vorkommt. Wenn das so ist, wird das Element aus der 1.
-            // Liste gelöscht.
-            if(isIn(liste2, liste1) && run){
-                liste1 = delElement("a", liste2, liste1);
-            }
-
-            // Es wird der Pointer auf das nächste ListenElement gesetzt und die Schleife
-            // wiederholt sich.
-            if(liste2 != null){
-                liste2 = liste2.getNext();
-            }
+        if(isIn(liste2, liste1)){
+            delA(liste2, liste1);
+            diffList(liste1, liste2.getNext());
+        } else {
+            diffList(liste1, liste2.getNext());
         }
 
         return liste1;
     }
 
-    // WICHTIG WICHTIG WICHTIG WICHTIG WICHTIG WICHTIG WICHTIG
+
     public boolean suffix(ListenElement s, ListenElement l){
-        boolean success = false;
-        boolean run = true;
-        ListenElement sbackup = s;
-        ListenElement lbackup = l;
 
-        // Die Schleife iteriert über die übergebene Liste l.
-        // bei jedem Element von l wir überprüft, ob es dem ersten Element aus
-        // s entspricht.
-        while(!success){
-            if(s.getData().equals(l.getData())){
-                ListenElement nextInnerELementS = s.getNext();
-                ListenElement nextInnerElementL = l.getNext();
-
-                // Sollte dieser Fall eintreten, springt die Methode in den inneren Schleifenblock
-                // und überprüft ab nun jedes folgenden Element von l mit jedem folgenden Element von s.
-                while(run){
-
-                    // sind alle Elemente 'gleich' und am Ende weisen beide Listen leere Listen (null) auf,
-                    // ist s ein suffix von l und die Methode gibt true zurück.
-                    if(nextInnerELementS == null && nextInnerElementL == null){
-                        run = false;
-                        success = true;
-                    }
-
-                    // Sobald die beiden aktuell überprüften Elemente nicht mehr gleich sind, bricht die
-                    // innere Schleife ab, da s zumindest bis zu diesem Zeitpunkt kein suffix von l sein kann.
-                    if(nextInnerELementS.getData().equals(nextInnerElementL.getData()) && run){
-                        nextInnerELementS = nextInnerElementL.getNext();
-                        nextInnerElementL = nextInnerElementL.getNext();
-                    } else {
-                        run = false;
-                    }
-                }
-
-                // Resett von s auf den Startzustand, setzen von l auf den Pointer der aktuell erreichten Position.
-                s = sbackup;
-                l = nextElementL;
-            }
-
-            l = l.getNext();
-        }
-
-        // Die Methode gibt den Inhalt der Boolean Variable success zurück.
-        return success;
     }
 
-
     public boolean infix(ListenElement i, ListenElement l){
-        boolean run = true;
-        boolean success = false;
 
-        ListenElement nextElementI = i;
-        ListenElement backupi = i;
-        ListenElement nextElementL = l;
-
-        // Die Schleife iteriert über die übergebene Liste l.
-        while(!success){
-
-               // Sobald das aktuell überprüfte Element aus l gleich dem ersten Element aus
-               // s ist, überprüft die Methode alle weiteren aufeinander folgenden Element
-               // beider Listen, ob diese gleich sind.
-               if(nextElementI.getData().equals(nextElementL.getData())){
-                   ListenElement nextInnerElementI = nextElementI.getNext();
-                   ListenElement nextInnerElementL = nextElementL.getNext();
-
-                   while(run){
-
-                        // Sollte i am ende eine Leereliste (null)
-                        // enthalten und alle vorheriegen Elemente waren identisch
-                        // verlässt die Methode die Schleife und gibt true zurück, da i ein infix von l ist.
-                       if(nextInnerElementI == null){
-                           run = false;
-                           success = true;
-                       }
-
-                       // Die Elemente aus i und l werden miteinander verglichen.
-                       if(nextInnerElementI.getData().equals(nextInnerElementL.getData()) && run){
-
-                            // Pointer auf die nächsten Elemente werden gesetzt.
-                           nextInnerElementI = nextInnerElementI.getNext();
-                           nextInnerElementL = nextInnerElementL.getNext();
-                       } else {
-                           run = false;
-                       }
-                   }
-               }
-
-               // Sollte i noch nicht am Ende sein, wird i resettet, so das bei dem
-               // nächsten durchlauf wieder das erste Element aus i mit dem aktuellen
-               // Element aus l verglichen werden kann.
-               i = backupi;
-               nextElementL = nextElementL.getNext();
-               run = true;
-        }
-
-        return success;
     }
 
     public boolean praefix(ListenElement p, ListenElement l){
-        boolean run = true;
-        boolean success = false;
-
-        // Es wird überprüft, ob das erste Element aus p gleich dem ersten Element aus l ist.
-        // Sollte dies der Fall sein, geht die Methode in die if Abfrage.
         if(p.getData().equals(l.getData())){
-            ListenElement nextElementL = l.getNext();
-            ListenElement nextElementP = p.getNext();
-
-            // Sollte das nächste Element der übergebenen p Liste null sein und alle davor gefolgenden
-            // Element waren identisch mit denen die in l überprüft wurden, gibt die Methode true
-            // zurück, da p ein Präfix von l ist.
-            while(run){
-                if(nextElementP == null){
-                    run = false;
-                    success = true;
-                }
-
-                // Jedes aktuelle Element aus l und p werden miteinander verglichen, sind sie
-                // indentisch werden die Pointer auf die darauf folgenden Elemente gesetzt.
-                // Sollten sie nicht identisch sein, wird die Schleife abgebrochen und die
-                // Methode gibt false zurück, da p kein Präfix von l ist.
-                if(nextElementP.getData().equals(nextElementL.getData())){
-                    nextElementL = nextElementL.getNext();
-                    nextElementP = nextElementP.getNext();
-                } else {
-                    run = false;
-                }
-            }
+            return true;
+        } else {
+            return false;
         }
-
-        return  success;
     }
 
-    //[fixed #2]
+    // [fixed #2]
     public ArrayList<Integer> eoCount(ListenElement l){
         boolean run = true;
         ArrayList<Integer> returnList = new ArrayList<>();
@@ -236,11 +108,13 @@ public class Main {
     }
 
 
-    // Die Methode entscheidet je nach übergebener Position, welche Teilmethode aufgerufen wird.
-    // e steht für: das erste Vorkommen des übergebene Elementes wird gelöscht
-    // l steht für: das letzte Vorkommen des übergebene Elementes wird gelöscht
-    // a steht für: alle Vorkommen des übergebene Elementes werden gelöscht.
+    // [Entscheidungs Methode]
     public ListenElement delElement(String p, ListenElement e, ListenElement l){
+        // Die Methode entscheidet je nach übergebener Position, welche Teilmethode aufgerufen wird.
+        // e steht für: das erste Vorkommen des übergebene Elementes wird gelöscht
+        // l steht für: das letzte Vorkommen des übergebene Elementes wird gelöscht
+        // a steht für: alle Vorkommen des übergebene Elementes werden gelöscht.
+
         ListenElement returnList = null;
 
         if(p.equals("e")){
@@ -260,7 +134,6 @@ public class Main {
 
         return returnList;
     }
-
 
     // [fixed #2]
     private ListenElement delE(ListenElement e, ListenElement returnList){
@@ -328,7 +201,7 @@ public class Main {
         return append(returnList, nextList);
     }
 
-
+    // [Entscheidungs Methode]
     public ListenElement substitute(String p, ListenElement e1, ListenElement e2, ListenElement returnList){
         if(p.equals("e")){
             returnList = substituteE(e1, e2, returnList);
@@ -343,125 +216,78 @@ public class Main {
         return returnList;
     }
 
-    // [fixed]
+    // [fixed 2]
     public ListenElement substituteE(ListenElement e1, ListenElement e2, ListenElement returnList){
-        boolean run = true;
+        boolean nextNull = false;
+        ListenElement nextList;
 
-        if(e1.getData().equals(returnList.getData())){
-            e1.setNext(returnList.getNext());
-            returnList = e1;
-            run = false;
+        // Sollte die leere Liste als Pointer übergeben worden sein, terminiert die Methode.
+        if(returnList == null){
+            nextNull = true;
         }
 
-        ListenElement startReturnList = returnList;
-
-        while(run){
-            ListenElement nextElement = returnList.getNext();
-            ListenElement savePrevious = returnList;
-
-            if(e1.getData().equals(nextElement.getData())){
-                changeElement(e2, savePrevious);
-
-                run = false;
-            }
-
-            if(!run){
-                returnList = nextElement;
-            }
-        }
-
-        return startReturnList;
-    }
-
-    // [fixed]
-    public ListenElement substituteL(ListenElement e1, ListenElement e2, ListenElement returnList){
-        boolean run = true;
-
-        //Spezialfall, falls das erste Element der Liste e1 entspricht und e1 in der restlichen Liste nicht mehr
-        //vorkommt
-        if(returnList.getData().equals(e1.getData()) && !isIn(e1, returnList)){
-            e2.setNext(returnList.getNext());
-            returnList = e2;
-
-            run = false;
-        }
-
-        ListenElement startReturnListe = returnList;
-
-        while(run){
-            ListenElement nextElement = returnList.getNext();
-            ListenElement savePrevious = returnList;
-
-            if(e1.getData().equals(nextElement.getData()) && !isIn(e2, nextElement)){
-                changeElement(e2, savePrevious);
-                run = false;
-            }
-
-            if(!run){
-                returnList = nextElement;
-            }
-        }
-
-        return startReturnListe;
-    }
-
-
-    public ListenElement substituteA(ListenElement e1, ListenElement e2, ListenElement returnList){
-        // Fehler
-
-        boolean run = true;
-
-        // Spezialfall falls das erste Element direkt getauscht werden muss
-        if(returnList.getData().equals(e1.getData())){
-            e2.setNext(returnList);
-            returnList = e2;
-        }
-
-        if(!isIn(e2, returnList)){
-            run = false;
-        }
-
-        while(run){
-            ListenElement nextElement = returnList.getNext();
-            ListenElement savePrevious = returnList;
-
-            if(nextElement == null){
-                run = false;
-            }
-
-            if(nextElement.getData().equals(e1.getData()) && run){
-                changeElement(e2, savePrevious);
-                if(!isIn(e2, returnList)){
-                    run = false;
-                }
-            }
-        }
-
-        return returnList;
-    }
-
-    /**
-     *
-     * @param e
-     * @return  Helper-Methode zum Löschen des nächsten Elemcentes vom übergebeben Element. Liefer true zurück,
-     *          wenn das Löschen erfolgreich war, ansonsten false. Sollte das Löschen nicht möglich sein, da
-     *          z.B. das nächste Element von dem übergebenen Element null ist, wird false zurück gegeben und
-     *          nichts verändern.
-     */
-    private boolean deleteSuccessor(ListenElement e){
-        ListenElement nextNextElement = null;
-
-        if(e.getNext() != null){
-            nextNextElement = e.getNext().getNext();
+        // Sollte das aktuelle Element gleich dem gesuchten Element sein, gibt die Methode
+        // eine Liste zurück, welche nicht mehr e1, sondern an dessen stelle e2 enthält.
+        if(returnList.getData().equals(e1.getData()) && !nextNull){
+            return append(e2, returnList.getNext());
         } else {
-            return false;
+            // Sollte dieser Fall nicht eintreten, wird die Methode rekursiv mit dem nächsten
+            // Element aufgerufen.
+            nextList = substituteE(e1, e2, returnList.getNext());
         }
 
-        e.setNext(nextNextElement);
-
-        return true;
+        // "Auf dem Rückweg" wird die erhaltene neue Liste mit dem vorherigen returnList Objekt appended
+        return append(returnList, nextList);
     }
 
+    // [fixed 2]
+    public ListenElement substituteL(ListenElement e1, ListenElement e2, ListenElement returnList){
+        boolean nextNull = false;
+        ListenElement nextList;
+
+        // Sollte die leere Liste als Pointer übergeben worden sein, terminiert die Methode.
+        if(returnList == null){
+            nextNull = true;
+        }
+
+        // Sollte das aktuelle Element gleich dem gesuchten Element sein und befindet sich in
+        // der weiteren Liste kein weiteres Vorkommen, gibt die Methode eine neue Liste zurück,
+        // in der das Element e1 mit dem Element e2 ausgetauscht wurde.
+        if(returnList.getData().equals(e1.getData()) && !nextNull && !isIn(e1, returnList)){
+            return append(e2, returnList.getNext());
+        } else {
+            // Sollte dieser Fall nicht eintreten, wird die Methode rekursiv erneut aufgerufen.
+            nextList = substituteE(e1, e2, returnList.getNext());
+        }
+
+        // "Auf dem Rückweg" wird die erhaltene neue Liste mit dem vorherigen returnList Objekt appended
+        return append(returnList, nextList);
+    }
+
+    // [fixed 2]
+    public ListenElement substituteA(ListenElement e1, ListenElement e2, ListenElement returnList){
+        boolean nextNull = false;
+        ListenElement nextList;
+
+        // Sollte die leere Liste als Pointer übergeben worden sein, terminiert die Methode.
+        if(returnList == null){
+            nextNull = true;
+        }
+
+        // Sollte das aktuelle Element gleich dem gesuchten Element sein, gibt die Methode
+        // nach einem rekursiven Aufruf von sich selber, eine appended Liste zurück, in der
+        // e1 mit e2 getauscht wurde.
+        if(returnList.getData().equals(e1.getData()) && !nextNull){
+            return substituteA(e1, e2, append(e2, returnList.getNext()));
+        } else {
+            // Sollten die Elemente nicht identisch sein, wird die Methode ohne Struktur Veränderungen
+            // erneut rekursiv aufgerufen.
+            nextList = substituteA(e1, e2, returnList.getNext());
+        }
+
+        // "Auf dem Rückweg" wird die erhaltene neue Liste mit dem vorherigen returnList Objekt appended
+        return append(returnList, nextList);
+    }
 
     //[fixed #2]
     private boolean isIn(ListenElement e, ListenElement l){
@@ -496,12 +322,6 @@ public class Main {
         return e;
     }
 
-
-    private void changeElement(ListenElement e2, ListenElement prev){
-        ListenElement nextNextElement = prev.getNext().getNext();
-        prev.setNext(e2);
-        prev.getNext().setNext(nextNextElement);
-    }
 
     public static void main(String[] args) {
         System.out.println("Hier kann dann alles ausgeführt werden!");
